2d14b954a836e2fae3cbcb72fbda13c7adc9e11e
jacopogrecodalceo <jacopo.grecodalceo@gmail.com>
2024-02-24 11:30:47 +0100

    readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # cordelia-ghost
-# cordelia-ghost
+
+Description of your CLI tool.
+
+positional arguments:
+audio_file						File to analyse
+
+options:
+-h, --help									show this help message and exit
+-t, --transparency							make transparency
+-i, --invert								make invert
+-g, --grain									make grain
+-e, --enhance								enhance
+-n, --axis									no axis
+-s ONSET, --onset ONSET						onset position
+-d DUR, --dur DUR							duration
+-o OUTPUT_DIRECTORY,--output_directory		OUTPUT_DIRECTORY output directory
+-c COLOR, --color COLOR						choose color
+-f FORMAT, --format FORMAT					choose format (png, pdf)
--- cordelia-ghost.py
+++ cordelia-ghost.py
@@ -7,18 +7,18 @@ def parse_arguments():
 	parser = argparse.ArgumentParser(description='Description of your CLI tool.')
 	parser.add_argument('audio_file', type=str, help='File to analyse')
  
-	parser.add_argument('-t', '--transparency', action='store_true', help='Make transparency')
-	parser.add_argument('-i', '--invert', action='store_true', help='Make invert')
-	parser.add_argument('-g', '--grain', action='store_true', help='Make grain')
-	parser.add_argument('-e', '--enhance', action='store_true', help='Enhance')
-	parser.add_argument('-n', '--axis', action='store_false', help='No axis')
+	parser.add_argument('-t', '--transparency', action='store_true', help='make transparency')
+	parser.add_argument('-i', '--invert', action='store_true', help='make invert')
+	parser.add_argument('-g', '--grain', action='store_true', help='make grain')
+	parser.add_argument('-e', '--enhance', action='store_true', help='enhance')
+	parser.add_argument('-n', '--axis', action='store_false', help='no axis')
  
-	parser.add_argument('-s', '--onset', type=float, help='Onset position', default=0.0)
-	parser.add_argument('-d', '--dur', type=float, help='Duration')
+	parser.add_argument('-s', '--onset', type=float, help='onset position', default=0.0)
+	parser.add_argument('-d', '--dur', type=float, help='duration')
  
-	parser.add_argument('-o', '--output_directory', type=str, help='Output directory')
-	parser.add_argument('-c', '--color', type=str, help='Choose color')
-	parser.add_argument('-f', '--format', type=str, help='Choose format', default='pdf')
+	parser.add_argument('-o', '--output_directory', type=str, help='output directory')
+	parser.add_argument('-c', '--color', type=str, help='choose color')
+	parser.add_argument('-f', '--format', type=str, help='choose format (png, pdf)', default='pdf')
 	#parser.add_argument('-o', '--option', type=int, default=10, help='Description of an option')
 	return parser.parse_args()
 
